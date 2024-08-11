045829fe87b3b1fa82ea287dd80d9d74fd9d9349
ali <a.alfares000@outlook.com>
2024-08-11 09:47:23 +0300

    Test 3

--- streamlit.py
+++ streamlit.py
@@ -1,10 +1,11 @@
 # importing libraries
 import streamlit as st
-import numpy as np 
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-import plotly.express as px 
+# import numpy as np 
+# import pandas as pd
+# import matplotlib.pyplot as plt
+# import seaborn as sns
+# import plotly.express as px 
+# import matplotlib.pyplot as plt
 
 
 st.title("مونوبولي")
